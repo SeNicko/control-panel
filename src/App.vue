686bbecbac6a673e6ad3dd42996a93e84fb1cd93
SeNicko <sebastianflajszer@gmail.com>
2021-01-03 16:46:49 +0100

    Basic fetch setup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	{{ radios }}
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref, onMounted } from "vue";
+import { apiUri } from "./config/api";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: "App",
+	setup() {
+		// Local state
+		const radios = ref([]);
+
+		// Get all radios from api
+		const getAllDevices = async () => {
+			try {
+				const response = await fetch("/radios");
+				const data = await response.json();
+				radios.value = data;
+			} catch (err) {
+				// TODO: handle in ui
+				console.error("An error has occured", err);
+			}
+		};
+
+		onMounted(() => {
+			// Fetch radios on app load
+			getAllDevices();
+		});
+
+		return {
+			// Return radios in order to acces them in component body
+			radios,
+		};
+	},
 });
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
